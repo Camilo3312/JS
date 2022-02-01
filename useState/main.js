4e60e39ffcb486bd8c976a5806707423a105df6a
camilo3312 <ccvelasquez531@misena.edu.co>
2022-01-31 20:24:47 -0500

    new project

---
+++ useState/main.js
@@ -0,0 +1,28 @@
+state = 0
+const useState = () => {
+    function setState(props) {
+        state = props
+        counter.textContent = state
+        if(state > 0) {
+            image.style.filter = `grayscale(${state})`
+        }
+        else {
+            image.style.filter = "none"
+        }
+    }
+    return {
+        setState
+    }
+}
+
+const { setState } = useState()
+
+const btn_increment = document.querySelector('.btn_increment')
+const btn_reset = document.querySelector('.btn_reset')
+const btn_decrement = document.querySelector('.btn_decrement')
+const counter = document.querySelector('.counter')
+const image = document.getElementById('image')
+
+btn_increment.addEventListener('click', () => setState(state + 1))
+btn_reset.addEventListener('click', () => setState(0))
+btn_decrement.addEventListener('click', () => setState(state - 1))
